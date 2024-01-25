4700e7ae5fc46510bdf2fe008172ba1076f6d2d1
Tae Yoon Kim <the.thinkty@gmail.com>
2024-01-25 13:21:13 -0500

    Added the sensor page for viewing individual sensors

---
+++ src/app/sensor/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { BackButton } from '@/components/backButton/BackButton'
+import { Graph } from '@/components/graph/Graph'
+import styles from './page.module.scss'
+
+export default function Sensor() {
+    const searchParams = useSearchParams()
+    const deviceId = searchParams.get('deviceId')
+
+    if (!deviceId) {
+        return (
+            <div className={styles.main}>
+                <div className={styles.head}>
+                    <div className={styles.title}>SUIT</div>
+                    <div className={styles.description}>Simple Unsecure IoT Platform</div>
+                </div>
+
+                <div>Error: No device ID</div>
+
+                <BackButton />
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.head}>
+                <div className={styles.title}>SUIT</div>
+                <div className={styles.description}>Simple Unsecure IoT Platform</div>
+            </div>
+
+            <Graph deviceId={deviceId} />
+
+            <BackButton />
+        </div>
+    )
+}
